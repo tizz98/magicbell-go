c33056a53551bb01b39115dec7cb9eca78d4fe7b
Elijah Wilson <dev.tizz98@gmail.com>
2021-02-07 16:51:33 -0500

    test users and gh actions

--- users_test.go
+++ users_test.go
@@ -84,23 +84,77 @@ func TestAPI_CreateUser(t *testing.T) {
 }
 
 func TestAPI_UpdateUser(t *testing.T) {
-	t.Run("200", func(t *testing.T) {
+	request := UpdateUserRequest{
+		ExternalID:       "56780",
+		Email:            "hana@magicbell.io",
+		FirstName:        "Hana",
+		LastName:         "Mohan",
+		CustomAttributes: nil,
+	}
+	userID := "7fb3ce9f-a866-4dff-8ce8-2f64f7c5ed4c"
 
+	t.Run("200", func(t *testing.T) {
+		runServer(t, "/users/7fb3ce9f-a866-4dff-8ce8-2f64f7c5ed4c", http.MethodPut, http.StatusOK, func(config Config) {
+			api := New(config)
+			user, err := api.UpdateUser(userID, request)
+
+			require.NoError(t, err)
+			require.NotNil(t, user)
+
+			assert.Equal(t, User{
+				ID:               "7fb3ce9f-a866-4dff-8ce8-2f64f7c5ed4c",
+				ExternalID:       "56780",
+				Email:            "hana@magicbell.io",
+				FirstName:        "Hana",
+				LastName:         "Mohan",
+				CustomAttributes: nil,
+			}, *user)
+		})
 	})
 
 	t.Run("400", func(t *testing.T) {
+		request := request
+		request.Email = ""
 
+		runServer(t, "/users/7fb3ce9f-a866-4dff-8ce8-2f64f7c5ed4c", http.MethodPut, http.StatusBadRequest, func(config Config) {
+			api := New(config)
+			user, err := api.UpdateUser(userID, request)
+
+			require.Error(t, err)
+			require.Nil(t, user)
+
+			assert.True(t, IsAPIErrors(err))
+
+			errs := err.(APIErrors)
+			assert.Len(t, errs, 1)
+			assert.Equal(t, APIError{
+				Code:    APIErrorCodeUserEmailNotProvided,
+				Message: "missing email",
+			}, errs[0])
+		})
 	})
 
 	t.Run("500", func(t *testing.T) {
+		runServer(t, "/users/7fb3ce9f-a866-4dff-8ce8-2f64f7c5ed4c", http.MethodPut, http.StatusInternalServerError, func(config Config) {
+			api := New(config)
+			user, err := api.UpdateUser(userID, request)
 
+			require.Error(t, err)
+			require.Nil(t, user)
+
+			assert.True(t, IsInternalServerError(err))
+			assert.Equal(t, InternalServerError{
+				StatusCode: 500,
+				Body:       "Internal server error\n",
+			}, err.(InternalServerError))
+		})
 	})
 }
 
 func TestCreateUser(t *testing.T) {
-
+	// TODO
 }
 
 func TestUpdateUser(t *testing.T) {
-
+	// TODO
 }
